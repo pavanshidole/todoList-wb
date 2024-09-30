c3b7145f7fdc45f5783809ab54f3adb6ac40dcbd
pavan shidole <pavanshidole@gmail.com>
2024-09-30 16:25:05 +0530

    work on todolist crud

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+const todoForm=document.getElementById("todoForm");
+
+const onTodoForm=(ele)=>{
+    ele.preventDefault();
+
+    let todoObj={
+        todoitem:
+    }
+}
+
+
+todoForm.addEventListener("submit" , onTodoForm);
